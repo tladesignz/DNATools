9deb3ea242f52cc67ff43d79367ca8f0dcb3f206
Benjamin Erhart <be@benjaminerhart.com>
2015-10-07 16:25:09 +0200

    Added ListAdapterHelper.

---
+++ src/com/netzarchitekten/tools/ListAdapterHelper.java
@@ -0,0 +1,158 @@
+/**
+ * DNA Android Tools.
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Die Netzarchitekten e.U., Benjamin Erhart
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.netzarchitekten.tools;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.widget.AdapterView;
+import android.widget.SimpleAdapter;
+
+/**
+ * This helper simplifies the handling of adapters and data Lists for ListViews.
+ * Use like this:
+ *
+ * <pre>
+ * ListAdapterHelper lah = new ListAdapterHelper(this,
+ *     R.layout.foo,
+ *     new String[] { &quot;foo&quot;, &quot;bar&quot; },
+ *     new int[] { R.id.foo, R.id.bar });
+ *
+ * mListView.setAdapter(lah.adapter);
+ *
+ * lah.add(&quot;baz&quot;, &quot;bam&quot;);
+ *
+ * lah.commit();
+ * </pre>
+ *
+ * @author Benjamin Erhart {@literal <berhart@netzarchitekten.com>}
+ */
+public class ListAdapterHelper {
+
+    public final SimpleAdapter adapter;
+
+    public final List<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+
+    private final Context mContext;
+
+    private final String[] mFrom;
+
+    public ListAdapterHelper(Context context, int resource, String[] from, int[] to) {
+        mContext = context;
+        mFrom = from;
+        adapter = new SimpleAdapter(context, data, resource, from, to);
+    }
+
+    /**
+     * Add a new HashMap entry to the List. Populate the HashMap with the values
+     * given using the keys from the constructor.
+     *
+     * @param strings
+     */
+    public void add(String... strings) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        int i = 0;
+
+        for (String string : strings) {
+            map.put(mFrom[i], string);
+            i++;
+        }
+
+        data.add(map);
+    }
+
+    /**
+     * Automatically converts resource IDs to strings.
+     *
+     * @see #add(String...)
+     * @param resIds
+     *            resource IDs
+     */
+    public void add(int... resIds) {
+        String[] strings = {};
+        List<String> newStrings = new ArrayList<String>();
+
+        for (int i = 0; i < resIds.length; i++) {
+            newStrings.add(mContext.getString(resIds[i]));
+        }
+
+        strings = newStrings.toArray(strings);
+
+        add(strings);
+    }
+
+    /**
+     * Convenience method. Often times, the first argument is a resource ID,
+     * while the others are strings.
+     *
+     * @param resId
+     *            a resource ID
+     * @param strings
+     *            more strings
+     */
+    public void add(int resId, String... strings) {
+        List<String> newStrings = new ArrayList<String>();
+
+        newStrings.add(mContext.getString(resId));
+
+        for (int i = 0; i < strings.length; i++) {
+            newStrings.add(strings[i]);
+        }
+
+        strings = newStrings.toArray(strings);
+
+        add(strings);
+    }
+
+    /**
+     * Clear the List.
+     */
+    public void clear() {
+        data.clear();
+    }
+
+    /**
+     * Notify the adapter of the changes.
+     */
+    public void commit() {
+        adapter.notifyDataSetChanged();
+    }
+
+    /**
+     * Returns the data associated with the given position.
+     *
+     * @see AdapterView.OnItemClickListener#onItemClick
+     * @param position
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    public Map<String, String> getItem(int position) {
+        return (Map<String, String>) adapter.getItem(position);
+    }
+}
